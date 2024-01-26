87f352d317d3032d4736fc5698f1e74c6096b097
Lenovo <Lenovo@DESKTOP-J07EOB4>
2024-01-26 12:32:38 -0300

    Adicionar arquivos de projeto.

---
+++ testando_mais_coisas/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace Home
+{
+    public class Home
+    {
+        static void Main(string[] args)
+        {
+            /*TESTANDO IF/ELSE
+             * int room = Convert.ToInt32(Console.ReadLine());
+             if (room > 1)
+             {
+                 Console.WriteLine("OK");
+             }
+             else
+             {
+                 Console.WriteLine("NOT OK");
+             }*/
+
+
+
+            /*TESTANDO SWITCHS
+             * bool bathroom = true;
+             switch (bathroom)
+             {
+                 case true: Console.WriteLine("IT'S OK");
+                     break;
+             }*/
+
+
+            /*TESTANDO FOR
+             *int livingroom = 3;
+             for (int i = 0; i < livingroom; i++)
+             {
+                 Console.WriteLine(i);
+             }*/
+
+
+
+
+            /* TESTANDO DO/WHILE
+             * int kitchen = 6;
+             while (kitchen < 6)
+             {
+                kitchen++;
+             }
+
+            int kitchen = 0;
+            do
+            {
+                kitchen++;
+            }
+            while (kitchen > 3);
+            Console.WriteLine(kitchen);*/
+
+           
+            
+            
+            /*TESTANDO ARRAYS E FOREACH
+          //  int[] backyard = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //Console.WriteLine(backyard[0]);
+            // Console.WriteLine(backyard.Length);
+           // Console.WriteLine(backyard.Sum());
+         /*  foreach (int i in backyard)
+            {
+                Console.WriteLine(i);
+            }*/
+        }
+    }
+}
